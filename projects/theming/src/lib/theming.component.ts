ae5ed67bc3eb2a2c126541e5924f18fc14e153b8
Char2s <Char2s@outlook.com>
2024-02-18 19:43:17 -0500

    feat: init library "theming"

---
+++ projects/theming/src/lib/theming.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-theming',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      theming works!
+    </p>
+  `,
+  styles: ``
+})
+export class ThemingComponent {
+
+}
---
+++ projects/theming/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of theming
+ */
+
+export * from './lib/theming.service';
+export * from './lib/theming.component';
