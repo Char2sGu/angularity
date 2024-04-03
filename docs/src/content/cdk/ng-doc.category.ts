db8ff6e3be695c4c58c2d88ad608158317ca199c
Char2s <Char2s@outlook.com>
2024-04-03 02:02:26 -0400

    feat(docs): expand all categories by default

--- docs/src/content/cdk/ng-doc.category.ts
+++ docs/src/content/cdk/ng-doc.category.ts
@@ -3,6 +3,7 @@ import { NgDocCategory } from '@ng-doc/core';
 const CdkCategory: NgDocCategory = {
   title: 'CDK',
   order: 2,
+  expanded: true,
 };
 
 export default CdkCategory;
--- docs/src/content/core/ng-doc.category.ts
+++ docs/src/content/core/ng-doc.category.ts
@@ -3,6 +3,7 @@ import { NgDocCategory } from '@ng-doc/core';
 const CoreCategory: NgDocCategory = {
   title: 'Core',
   order: 1,
+  expanded: true,
 };
 
 export default CoreCategory;
