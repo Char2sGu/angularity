f647e4cc8d9dd5635c5d5571ee2af893f5ed0c62
Char2s <Char2s@outlook.com>
2024-06-28 13:38:37 +0800

    feat(cdk): replace `createDialogConfig` with `useDialog`

--- packages/cdk/dialog/src/dialog.ts
+++ packages/cdk/dialog/src/dialog.ts
@@ -1,4 +1,7 @@
-import { DialogConfig, DialogRef } from '@angular/cdk/dialog';
+import { Dialog, DialogConfig, DialogRef } from '@angular/cdk/dialog';
+import { DestroyRef, inject, Injector, Signal, Type } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, merge, of, Subject, switchMap } from 'rxjs';
 
 const TYPES = Symbol('TYPES');
 
@@ -22,8 +25,35 @@ export type DialogConfigOf<T extends DialogIoTypes<any, any>> = DialogConfig<
   DialogRefOf<T>
 >;
 
-export function createDialogConfig<T extends DialogIoTypes<any, any>>(
-  config: DialogConfigOf<T>,
-): typeof config {
-  return config;
+export interface DialogFacade<T extends DialogIoTypes<any, any>> {
+  launch: (config?: DialogConfigOf<T>) => DialogRefOf<T>;
+  ref: Signal<DialogRefOf<T> | undefined>;
 }
+
+export const useDialog = <T extends DialogIoTypes<any, any>>(
+  component: Type<T>,
+  configDefaults: Partial<DialogConfigOf<T>> = {},
+  [service, injector, destroyRef] = [
+    inject(Dialog),
+    inject(Injector),
+    inject(DestroyRef),
+  ],
+): DialogFacade<T> => {
+  const launched$ = new Subject<DialogRefOf<T>>();
+  destroyRef.onDestroy(() => launched$.complete());
+  return {
+    launch: (config: DialogConfigOf<T> = {}): DialogRefOf<T> => {
+      const open = service.open.bind(service);
+      const ref = open(component, { injector, ...configDefaults, ...config });
+      launched$.next(ref);
+      return ref;
+    },
+    ref: toSignal(
+      launched$.pipe(
+        switchMap((ref) =>
+          merge(of(ref), ref.closed.pipe(map(() => undefined))),
+        ),
+      ),
+    ),
+  };
+};
