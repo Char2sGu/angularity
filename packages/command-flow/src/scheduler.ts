5252a4418409fd893dd7e76011e2aed69f45afe5
Char2s <Char2s@outlook.com>
2024-11-24 14:56:52 -0500

    feat(command-flow): use setImmediate as default scheduler

--- packages/command-flow/src/scheduler.ts
+++ packages/command-flow/src/scheduler.ts
@@ -2,19 +2,33 @@ import { forwardRef, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
-  useExisting: forwardRef(() => AnimationFrameBasedCommandFlowScheduler),
+  useExisting: forwardRef(() => SetImmediateCommandFlowScheduler),
 })
 export abstract class CommandFlowScheduler {
   abstract next(fn: () => void): void;
 }
 
-@Injectable({
-  providedIn: 'root',
-})
+/**
+ * Implementation of {@link CommandFlowScheduler} that uses
+ * `setImmediate` to schedule the next execution.
+ */
+@Injectable({ providedIn: 'root' })
+export class SetImmediateCommandFlowScheduler implements CommandFlowScheduler {
+  next(fn: () => void): void {
+    setImmediate(fn);
+  }
+}
+
+/**
+ * Implementation of {@link CommandFlowScheduler} that uses
+ * `requestAnimationFrame` to schedule the next execution.
+ * Available only in the browser platform.
+ */
+@Injectable({ providedIn: 'root' })
 export class AnimationFrameBasedCommandFlowScheduler
   implements CommandFlowScheduler
 {
   next(fn: () => void): void {
-    requestAnimationFrame(fn);
+    window.requestAnimationFrame(fn);
   }
 }
