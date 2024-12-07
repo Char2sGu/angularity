c23fd61f09f1c9b3d5acd3f39dfa91f2696c4b17
Char2s <Char2s@outlook.com>
2024-12-07 16:18:41 -0500

    build(release): use custom release script

---
+++ scripts/release.mjs
@@ -0,0 +1,54 @@
+import { execSync } from 'node:child_process';
+import { argv } from 'node:process';
+import {
+  releaseVersion,
+  releaseChangelog,
+  releasePublish,
+} from 'nx/release/index.js';
+import { simpleGit } from 'simple-git';
+
+main().then(() => process.exit(0));
+
+async function main() {
+  const [, , specifier, preid] = argv;
+  if (!specifier) throw new Error('Missing required arguments');
+
+  const repository = simpleGit();
+
+  const version = await releaseVersion({
+    ...(preid
+      ? {
+          specifier: 'pre' + specifier,
+          preid,
+        }
+      : {
+          specifier,
+        }),
+    projects: ['packages/*'],
+    /**@satisfies {import('nx/src/command-line/release/version').ReleaseVersionGeneratorSchema } */
+    generatorOptionsOverrides: {
+      // The built-in lock file update is problematic and brings tons of
+      // irrelevant changes.
+      skipLockFileUpdate: true,
+    },
+    gitCommit: false,
+    gitTag: false,
+  }).then((r) => r.workspaceVersion);
+
+  execSync('npm install --package-lock-only');
+  await repository.add('package-lock.json');
+
+  await releaseChangelog({
+    version,
+    gitCommit: false,
+    gitTag: false,
+  });
+
+  await repository.add('CHANGELOG.md');
+
+  await repository
+    .commit(`build(release): v${version}`)
+    .addAnnotatedTag(`v${version}`, `Release v${version}`);
+
+  await releasePublish({ access: 'public' });
+}
