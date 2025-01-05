cbd73d6eb00df83d21f74f1c8139e9f4492f60f0
Char2s <Char2s@outlook.com>
2025-01-06 00:48:18 +0800

    refactor(elements): throw error when ELEMENT_REGISTRY injected on non-browser platform

--- packages/elements/src/element-registry.ts
+++ packages/elements/src/element-registry.ts
@@ -1,11 +1,19 @@
-import { InjectionToken } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, InjectionToken, PLATFORM_ID } from '@angular/core';
 
 /**
  * Injection Token for the ECMA `CustomElementRegistry` instance.
+ * @remarks Injecting this token on non-browser platforms will result in an error.
  * @see https://developer.mozilla.org/en-US/docs/Web/API/CustomElementRegistry
  */
-
 export const ELEMENT_REGISTRY = new InjectionToken<CustomElementRegistry>(
   'ELEMENT_REGISTRY',
-  { factory: () => window.customElements },
+  {
+    factory: () => {
+      const platform = inject(PLATFORM_ID);
+      if (!isPlatformBrowser(platform))
+        throw new Error(`illegal access of ELEMENT_REGISTRY`);
+      return window.customElements;
+    },
+  },
 );
