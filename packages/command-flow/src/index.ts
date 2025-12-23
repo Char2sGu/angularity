0d42d9c8d83dc3b585752a7a9b267aa58d43c132
Char2s <Char2s@outlook.com>
2025-12-23 01:07:09 -0500

    fix(command-flow): add missing flow exports

--- packages/command-flow/src/index.ts
+++ packages/command-flow/src/index.ts
@@ -2,6 +2,7 @@ export * from './buses';
 export * from './core';
 export * from './facade';
 export * from './factories';
+export * from './flow';
 export * from './handlers';
 export * from './pseudo-type';
 export * from './scheduler';
